de656794a32b72bf72bb02b6b26e495da3f4652f
danielsteman <daniel-steman@live.nl>
2023-03-23 19:12:20 +0700

    login logic wip

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 
+import axios from 'axios';
+
 const API_URL = "http://localhost:8000/login";
+const SESSION_URL = "http://localhost:8000/session";
 
 async function login() {
-  const redirect = await (await fetch(API_URL)).json();
-  const url = redirect.url
+  const redirect = await axios.get(API_URL, { withCredentials: true });
+  const url = redirect.data.url
   window.location.href = url
+}
+
+async function session() {
+  const res = await axios.get(SESSION_URL, { withCredentials: true });
+  console.log(document.cookie)
 }
 
 </script>
@@ -14,6 +22,9 @@ async function login() {
   <div>
     <button @click="login">
       Login
+    </button>
+    <button @click="session">
+      Check session
     </button>
   </div>
 </template>
