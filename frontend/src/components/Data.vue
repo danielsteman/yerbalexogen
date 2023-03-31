a453fa2a842e470f48a278dd7e87ea6e8ef65cea
danielsteman <daniel-steman@live.nl>
2023-03-31 13:54:57 +0700

    add form to fetch some example data

--- frontend/src/components/Data.vue
+++ frontend/src/components/Data.vue
@@ -1,20 +1,35 @@
 <script setup lang="ts">
 
 import axios from 'axios';
+import { ref } from 'vue';
 
 const API_URL = "http://localhost:8000/data/hrv-bulk";
 
 async function getData() {
-  const res = await axios.get(API_URL, { params: { session_id: localStorage.getItem("sessionId") } });
+  const params = {
+    session_id: localStorage.getItem("sessionId"),
+    start_date: "2022-01-01",
+    end_date: "2022-01-10",
+  };
+  const res = await axios.get(API_URL, { params: params });
   console.log(res)
 }
 
 </script>
 
+<script lang="ts">
+
+const startDate = ref("yyyy-mm-dd");
+const endDate = ref("yyyy-mm-dd");
+
+</script>
+
 <template>
   <div>
-    <button @click="getData">
-      Get heartrate intraday data
-    </button>
+    <form @click="getData">
+      <input v-model="startDate" />
+      <input v-model="endDate" />
+      <button type="submit" value="Get data">Get data</button>
+    </form>
   </div>
 </template>
